f85ba54e30299e92a40840579d06abb9a13e952e
Matthew Gleich <matthewgleich@gmail.com>
2020-09-18 11:19:30 -0400

    👷 Add prettier
    Signed-off-by: Matthew Gleich <matthewgleich@gmail.com>

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
@@ -18,5 +18,5 @@ export default function Home() {
         </h2>
       </main>
     </div>
-  );
+  )
 }
