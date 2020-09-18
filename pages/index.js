fa0b65ed07681bab4ed9f365a925461727d4378c
Matthew Gleich <matthewgleich@gmail.com>
2020-09-18 11:59:12 -0400

    ✨ Add link to GitHub account and old site
    Signed-off-by: Matthew Gleich <matthewgleich@gmail.com>

--- pages/index.js
+++ pages/index.js
@@ -4,19 +4,42 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div className={styles.container}>
-      <Head>
-        <title>👋 Matthew Gleich</title>
-        <link rel="icon" href="/logo.png" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>👋 Hello!</h1>
-        <h2 className={styles.description}>
-          🚧 I'm currently{' '}
-          <a href="https://github.com/Matt-Gleich/site-v2">building</a> this
-          site 🚧
-        </h2>
-      </main>
+      <Header />
+      <Main />
     </div>
+  )
+}
+
+const Header = () => {
+  return (
+    <Head>
+      <title>👋🏼 Matthew Gleich</title>
+      <link rel="icon" href="/logo.png" />
+    </Head>
+  )
+}
+
+const Main = () => {
+  return (
+    <main className={styles.main}>
+      <h1 className={styles.title}> 👋🏼 Hello!</h1>
+      <h2 className={styles.description}>
+        🚧 I'm currently{' '}
+        <a href="https://github.com/Matt-Gleich/site-v2">building</a> this site
+        🚧
+      </h2>
+      <p>
+        In the mean time you can go checkout my{' '}
+        <a href="https://github.mattglei.ch" target="_blank">
+          GitHub account
+        </a>{' '}
+        <br />
+        or my{' '}
+        <a href="https://old.mattglei.ch" target="_blank">
+          old site built with flutter
+        </a>
+        .
+      </p>
+    </main>
   )
 }
