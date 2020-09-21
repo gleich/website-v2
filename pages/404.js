347766c317020672efc07e76a0723b39d94e4de4
Matthew Gleich <email@mattglei.ch>
2020-09-21 02:14:23 -0400

    ✨ Change title on 404 page
    Signed-off-by: Matthew Gleich <email@mattglei.ch>

--- pages/404.js
+++ pages/404.js
@@ -1,18 +1,26 @@
 import styles from '../styles/404.module.css'
+import Head from 'next/head'
 
 export default function Custom404() {
   return (
-    <main className={styles.body}>
-      <img
-        className={styles.lostGif}
-        src="https://media.giphy.com/media/Bp3dFfoqpCKFyXuSzP/giphy.gif"
-        alt="Seth Meyers lost"
-      />
-      <h1>❗️ 404 Error ❗️</h1>
-      <h1 className={styles.title}>Whoa there partner!</h1>
-      <div className={styles.subtitle}>
-        <h2>You left the system</h2>
-      </div>
-    </main>
+    <div>
+      <Head>
+        <title>
+          ahhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh
+        </title>
+      </Head>
+      <main className={styles.body}>
+        <img
+          className={styles.lostGif}
+          src="https://media.giphy.com/media/Bp3dFfoqpCKFyXuSzP/giphy.gif"
+          alt="Seth Meyers lost"
+        />
+        <h1>❗️ 404 Error ❗️</h1>
+        <h1 className={styles.title}>Whoa there partner!</h1>
+        <div className={styles.subtitle}>
+          <h2>You left the system</h2>
+        </div>
+      </main>
+    </div>
   )
 }
