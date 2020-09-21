c576a9eb757d49fe1136b0a174fa2447bef05e59
Matthew Gleich <email@mattglei.ch>
2020-09-21 01:54:06 -0400

    🐛 Fix highlight color
    Signed-off-by: Matthew Gleich <email@mattglei.ch>

--- pages/404.js
+++ pages/404.js
@@ -9,7 +9,7 @@ export default function Custom404() {
         alt="Seth Meyers lost"
       />
       <h1>❗️ 404 Error ❗️</h1>
-      <h1 className={styles.title}>Woah there partner!</h1>
+      <h1 className={styles.title}>Whoa there partner!</h1>
       <div className={styles.subtitle}>
         <h2>You left the system</h2>
       </div>
