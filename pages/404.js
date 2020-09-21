3eadcae68580bc8cb127cd1097660222dbfec23e
Matthew Gleich <email@mattglei.ch>
2020-09-21 01:26:10 -0400

    ✨ Add gif to 404 page
    Signed-off-by: Matthew Gleich <email@mattglei.ch>

--- pages/404.js
+++ pages/404.js
@@ -3,10 +3,15 @@ import styles from '../styles/404.module.css'
 export default function Custom404() {
   return (
     <main className={styles.body}>
+      <img
+        className={styles.lostGif}
+        src="https://media.giphy.com/media/Bp3dFfoqpCKFyXuSzP/giphy.gif"
+        alt="Seth Meyers lost"
+      />
       <h1>❗️ 404 Error ❗️</h1>
       <h1 className={styles.title}>Woah there partner!</h1>
       <div className={styles.subtitle}>
-        <h2>You left the system!</h2>
+        <h2>You left the system</h2>
       </div>
     </main>
   )
