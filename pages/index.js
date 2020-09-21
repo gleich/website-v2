f5d71507a24e2fde374a3d4967571c9cf5e38e4f
Matthew Gleich <email@mattglei.ch>
2020-09-21 00:43:38 -0400

    🗑 Remove use of removed container class
    Signed-off-by: Matthew Gleich <email@mattglei.ch>

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Header />
       <Main />
     </div>
