a73bab7cb5c933b4105c8333ced873b440e17d17
Matthew Gleich <email@mattglei.ch>
2020-09-21 00:37:01 -0400

    ♻️ Refactor to use proper favicons
    Signed-off-by: Matthew Gleich <email@mattglei.ch>

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,27 @@ const Header = () => {
   return (
     <Head>
       <title>👋🏼 Matthew Gleich</title>
-      <link rel="icon" href="/me.png" />
+
+      <link rel="icon" href="/favicon.ico" />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/site.webmanifest" />
+
       <meta
         name="description"
         content="Matthew Gleich is a 16-year-old thats building scalable, multi-platform applications using cutting edge frameworks and technologies. 100% self taught 🚀"
