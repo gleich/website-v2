d67d78704f75438e435602f9ba0c141fae0cd91b
Matthew Gleich <git@mattglei.ch>
2021-04-24 16:06:52 -0400

    refactor: have herd use styled components
    Signed-off-by: Matthew Gleich <git@mattglei.ch>

--- src/components/pages/herd/herd.tsx
+++ src/components/pages/herd/herd.tsx
@@ -1,12 +1,34 @@
 import { ReactElement } from 'react'
-import styles from '../../../styles/components/pages/herd/HerdOfEmojis.module.css'
+import styled, { keyframes } from 'styled-components'
+
+const rotatingAnimation = keyframes`
+  from {
+    transform: rotate(0deg);
+    -webkit-transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+    -webkit-transform: rotate(360deg);
+  }
+`
+
+const Emoji = styled.div`
+  animation: ${rotatingAnimation} 4s linear infinite;
+  -webkit-animation: ${rotatingAnimation} 4s linear infinite;
+  font-size: 2rem;
+`
+
+const Herd = styled.div`
+  display: inline-flex;
+  flex-direction: column;
+`
 
 export function HerdOfEmojis(): ReactElement {
   const emojis = ['🐢', '🐄', '🦎']
   const column: JSX.Element[] = []
   for (let i = 0; i < 9; i++) {
     const emoji = emojis[Math.floor(Math.random() * Math.floor(emojis.length))]
-    column.push(<div className={styles.emoji}> {emoji} </div>)
+    column.push(<Emoji>{emoji}</Emoji>)
   }
-  return <div className={styles.herd}>&nbsp;{column}</div>
+  return <Herd>&nbsp;{column}</Herd>
 }
