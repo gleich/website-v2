b3d74438f3c46b40544d1f913bb303f8fbb0cf73
Matt Gleich <git@mattglei.ch>
2021-09-08 18:55:41 -0400

    feat: repo redirects at /gh/
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- next.config.js
+++ next.config.js
@@ -6,4 +6,14 @@ module.exports = {
     config.resolve.alias['~'] = path.resolve('./src')
     return config
   },
+
+  async redirects() {
+    return [
+      {
+        source: '/gh/:name*',
+        destination: 'https://github.com/gleich/:name*',
+        permanent: true,
+      },
+    ]
+  },
 }
