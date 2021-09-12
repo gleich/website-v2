91c8f8004752db4d3968425d99e03abb28f81fbd
Matt Gleich <git@mattglei.ch>
2021-09-11 20:52:14 -0400

    feat: get social media accounts
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,30 +2,44 @@ import Center from '@components/Center'
 import Copyright from '@components/Copyright'
 import GradientUnderline from '@components/GradientUnderline'
 import Meta from '@components/Meta'
-import type { NextPage } from 'next'
+import getSocials from '@data/socials'
+import type { GetStaticProps, InferGetStaticPropsType, NextPage } from 'next'
 
-const Index: NextPage = () => (
-  <>
-    <Meta
-      title="Matt Gleich"
-      description="17-year-old open-source developer from New Hampshire"
-    />
-    <main>
-      <Center root>
-        <div style={{ display: 'flex', flexDirection: 'column' }}>
-          <GradientUnderline>
-            <h2>Hey there, I&apos;m</h2>
-          </GradientUnderline>
-          <GradientUnderline direction="left" delay={3}>
-            <h1>Matt Gleich</h1>
-          </GradientUnderline>
-        </div>
-      </Center>
-    </main>
-    <footer>
-      <Copyright />
-    </footer>
-  </>
-)
+const Index: NextPage = ({
+  accounts,
+}: InferGetStaticPropsType<typeof getStaticProps>) => {
+  console.log(accounts)
+  return (
+    <>
+      <Meta
+        title="Matt Gleich"
+        description="17-year-old open-source developer from New Hampshire"
+      />
+      <main>
+        <Center root>
+          <div style={{ display: 'flex', flexDirection: 'column' }}>
+            <GradientUnderline>
+              <h2>Hey! I&apos;m</h2>
+            </GradientUnderline>
+            <GradientUnderline direction="left" delay={2}>
+              <h1>Matt Gleich</h1>
+            </GradientUnderline>
+          </div>
+        </Center>
+      </main>
+      <footer>
+        <Copyright />
+      </footer>
+    </>
+  )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const accounts = await getSocials()
+  return {
+    props: { accounts },
+    revalidate: 3600,
+  }
+}
 
 export default Index
