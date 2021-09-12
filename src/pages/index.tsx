e2bbd9d5d0913b35b1bd903863ad77599efe6378
Matt Gleich <git@mattglei.ch>
2021-09-12 01:28:45 -0400

    feat: break page into components
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,11 @@
 import Center from '@components/Center'
 import Copyright from '@components/Copyright'
-import GradientUnderline from '@components/GradientUnderline'
 import Meta from '@components/Meta'
 import getSocials from '@data/socials'
 import type { GetStaticProps, InferGetStaticPropsType, NextPage } from 'next'
-import styles from '@styles/index.module.css'
-import TextLoop from 'react-text-loop'
-import time from '@data/time'
+import Top from '@components/pages/index/Top'
+import Name from '@components/pages/index/Name'
+import Description from '@components/pages/index/Description'
 
 const Index: NextPage = ({
   accounts,
@@ -18,31 +17,20 @@ const Index: NextPage = ({
         title="Matt Gleich"
         description="17-year-old open-source developer from New Hampshire"
       />
+      <style jsx>
+        {`
+          .left {
+            display: flex;
+            flex-direction: column;
+          }
+        `}
+      </style>
       <main>
         <Center root>
-          <div className={styles.left}>
-            <GradientUnderline>
-              <h2>
-                {' '}
-                <span className={styles.wave}>👋🏼</span>
-                <span className={styles.hey}>Hey! I&apos;m</span>
-              </h2>
-            </GradientUnderline>
-            <GradientUnderline direction="left" delay={2}>
-              <h1 className={styles.name}>Matt Gleich</h1>
-            </GradientUnderline>
-            <h4 className={styles.description}>
-              {time.full}{' '}
-              <TextLoop delay={500} mask>
-                <span>developer</span>
-                <span>designer</span>
-                <span>photographer</span>
-                <span>cyclist</span>
-                <a href="https://hackclub.com" target="_blank" rel="noreferrer">
-                  hackclubber
-                </a>
-              </TextLoop>
-            </h4>
+          <div className="left">
+            <Top />
+            <Name />
+            <Description />
           </div>
         </Center>
       </main>
