77ca156dae430b9dd35db9473fda3f131b10e67a
Matt Gleich <git@mattglei.ch>
2021-09-12 15:23:02 -0400

    feat: social media icons
    Signed-off-by: Matt Gleich <git@mattglei.ch>

---
+++ src/components/pages/index/AccountIcon.tsx
@@ -0,0 +1,41 @@
+import Image from 'next/image'
+
+const AccountIcon = ({
+  name,
+  url,
+  imagePath,
+}: {
+  name: string
+  url: string
+  imagePath: string
+}): JSX.Element => {
+  return (
+    <>
+      <style jsx>
+        {`
+          .icon {
+            filter: invert(100%) saturate(0%);
+            -moz-user-select: -moz-none;
+            -khtml-user-select: none;
+            -webkit-user-select: none;
+            -ms-user-select: none;
+            user-select: none;
+          }
+
+          @media (prefers-color-scheme: light) {
+            .icon {
+              filter: none;
+            }
+          }
+        `}
+      </style>
+      <div className="icon" title={name}>
+        <a href={url} rel={`Matt Gleich's ${name}`} target="_blank">
+          <Image src={imagePath} width="40px" height="40px" alt={name} />
+        </a>
+      </div>
+    </>
+  )
+}
+
+export default AccountIcon
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,16 @@
 import Center from '@components/Center'
 import Copyright from '@components/Copyright'
 import Meta from '@components/Meta'
-import getSocials from '@data/socials'
+import getSocials, { SocialMediaAccount } from '@data/socials'
 import type { GetStaticProps, InferGetStaticPropsType, NextPage } from 'next'
 import Top from '@components/pages/index/Top'
 import Name from '@components/pages/index/Name'
 import Description from '@components/pages/index/Description'
+import AccountIcon from '@components/pages/index/AccountIcon'
 
 const Index: NextPage = ({
   accounts,
 }: InferGetStaticPropsType<typeof getStaticProps>) => {
-  console.log(accounts)
   return (
     <>
       <Meta
@@ -19,21 +19,52 @@ const Index: NextPage = ({
       />
       <style jsx>
         {`
+          main {
+            display: flex;
+            flex-direction: row;
+          }
+
+          /* Work on making this responsive */
+
           .left {
             display: flex;
             flex-direction: column;
           }
+
+          .accounts {
+            display: flex;
+            flex-direction: column;
+            margin-left: 50px;
+            margin-top: 50px;
+          }
+
+          .accounts > div {
+            margin-bottom: 10px !important;
+          }
+
+          .accounts:first-child {
+            margin-bottom: 0 !important;
+          }
         `}
       </style>
-      <main>
-        <Center root>
+      <Center root>
+        <main>
           <div className="left">
             <Top />
             <Name />
             <Description />
           </div>
-        </Center>
-      </main>
+          <div className="right">
+            <div className="accounts">
+              {accounts.map((account: SocialMediaAccount) => (
+                <div key={account.name}>
+                  <AccountIcon {...account} />
+                </div>
+              ))}
+            </div>
+          </div>
+        </main>
+      </Center>
       <footer>
         <Copyright />
       </footer>
