999f2f68bfa5bd51b7afce342836a0e2e68bca11
Matt Gleich <git@mattglei.ch>
2021-10-06 22:58:43 -0400

    feat: add redirects for /photography and /projects
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- next.config.js
+++ next.config.js
@@ -14,6 +14,17 @@ module.exports = {
         destination: 'https://github.com/gleich/:name*',
         permanent: true,
       },
+      {
+        source: '/photography',
+        destination:
+          'https://lightroom.adobe.com/shares/e96b417d28384fe7b26b54b8809f5d08',
+        permanent: true,
+      },
+      {
+        source: '/projects',
+        destination: 'https://github.com/gleich?tab=repositories',
+        permanent: true,
+      },
     ]
   },
 }
