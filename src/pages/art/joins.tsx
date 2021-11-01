8a2d714bc0d4003a15331d40c4a02d7dac58c087
Matt Gleich <git@mattglei.ch>
2021-11-01 01:17:59 -0400

    feat: joins art
    Signed-off-by: Matt Gleich <git@mattglei.ch>

---
+++ src/pages/art/joins.tsx
@@ -0,0 +1,51 @@
+import Center from '@components/Center'
+import Meta from '@components/Meta'
+import LogoBlock from '@components/pages/art/joins/Block'
+import { NextPage } from 'next'
+import Link from 'next/link'
+
+const Logo: NextPage = (): JSX.Element => {
+  const squares = 50
+
+  const blocks: JSX.Element[] = []
+  for (let i = 0; i < squares; i++) {
+    blocks.push(<LogoBlock index={i} total={squares} />)
+  }
+
+  return (
+    <>
+      <style jsx>
+        {`
+          .stack {
+            display: grid;
+            width: fit-content;
+            min-height: 50vh;
+            min-width: 50vw;
+            padding: 20px;
+          }
+
+          main {
+            max-height: 98vh;
+          }
+
+          p {
+            position: absolute;
+            right: 20px;
+            bottom: 20px;
+          }
+        `}
+      </style>
+      <Meta title="Joins" description="Generative art piece by Matt Gleich" />
+      <Center root>
+        <main>
+          <div className={'stack'}>{blocks}</div>
+          <p>
+            Joins by <Link href="/">Matt Gleich</Link>
+          </p>
+        </main>
+      </Center>
+    </>
+  )
+}
+
+export default Logo
