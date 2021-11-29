240388558d7d6f2b309465d78db69c8ecf52246f
Matt Gleich <git@mattglei.ch>
2021-11-28 21:19:57 -0500

    style: make sections more compact
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- src/components/pages/index/Section.tsx
+++ src/components/pages/index/Section.tsx
@@ -16,7 +16,7 @@ const Section = ({
           .section {
             display: flex;
             flex-direction: column;
-            margin: 100px 0;
+            margin: 60px 0;
           }
 
           .name {
