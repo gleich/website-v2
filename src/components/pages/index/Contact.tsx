68632b1f0da8e72ed7252c9d9036489917ab8daa
Matt Gleich <git@mattglei.ch>
2021-12-06 10:41:03 -0500

    feat(minor): change email text to real link
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- src/components/pages/index/Contact.tsx
+++ src/components/pages/index/Contact.tsx
@@ -3,10 +3,11 @@ import Section from './Section'
 
 const Contact = (): JSX.Element => (
   <Section name="contact">
-    I would love to hear from you! Email me at email at mattgleich.com or DM me
-    on <Route href="https://twitter.com/matt_gleich">twitter</Route>. I&apos;ll
-    get back to you as soon as possible but depending on the time of year I
-    might be a little late to respond.
+    I would love to hear from you! Email me at{' '}
+    <a href="mailto:email@mattglei.ch">email@mattglei.ch</a> or DM me on{' '}
+    <Route href="https://twitter.com/matt_gleich">twitter</Route>. I&apos;ll get
+    back to you as soon as possible but depending on the time of year I might be
+    a little late to respond.
   </Section>
 )
 
